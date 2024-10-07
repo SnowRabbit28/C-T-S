0b3d920e3501d2bc23c4f64a112f20189ad5fe86
Snow_Rabbit <vega2828@naver.com>
2024-10-07 17:49:35 +0900

    [level 0] Title: 문자열 돌리기, Time: 16.19 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+            
+        foreach(Char a in s)
+        {
+            Console.WriteLine(a);
+        }
+
+    }
+}
