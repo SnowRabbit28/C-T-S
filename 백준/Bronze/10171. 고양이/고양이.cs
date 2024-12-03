9b9a23b331b312e9d40438d72a17ff142b6051a9
Snow_Rabbit <vega2828@naver.com>
2024-12-03 22:24:55 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5328 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,12 @@
+using System;
+
+class test
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
