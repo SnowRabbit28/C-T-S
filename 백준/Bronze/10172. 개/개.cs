b02fe5acdd744ac6e5f68a20f8397e1c52640cd7
Snow_Rabbit <vega2828@naver.com>
2024-12-04 21:03:58 +0900

    [Bronze V] Title: 개, Time: 44 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,15 @@
+using System;
+
+class test
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
+
+
