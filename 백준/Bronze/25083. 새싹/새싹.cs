ac11714d6828d064bf9c55cf8bf06a53ea64ad0b
Snow_Rabbit <vega2828@naver.com>
2024-12-06 20:49:29 +0900

    [Bronze V] Title: 새싹, Time: 48 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,14 @@
+using System;
+
+class test
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("         ,r\'\"7");
+        Console.WriteLine("r`-_   ,\'  ,/");
+        Console.WriteLine(" \\. \". L_r\'");
+        Console.WriteLine("   `~\\/");
+        Console.WriteLine("      |");
+        Console.WriteLine("      |");
+    }
+}
