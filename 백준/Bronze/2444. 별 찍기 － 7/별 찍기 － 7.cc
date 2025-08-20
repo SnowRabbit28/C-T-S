a1a74c42c41b143f95f9250a2c54bf1493720dd0
Snow_Rabbit <vega2828@naver.com>
2025-08-20 19:03:39 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -11,34 +11,20 @@ int main()
 
     for (int i = 1; i <= N - 1; i++)
     {
-        for (int j = 1; j <= N - i; j++)
-        {
-            cout << ' ';
-        }
-        for (int j = 1; j <= 2 * i - 1; j++)
-        {
-            cout << '*';
-        }
-        cout << '\n';
+        cout << string(N - i, ' ')
+             << string(2 * i - 1, '*')
+            << '\n';
     }
-
-    for (int i = 1; i <= 2 * N - 1; i++)
+    for (int i = 0; i < 2 * N - 1; i++)
     {
         cout << "*";
     }
-
     cout << '\n';
-
     for (int i = 1; i <= N - 1; i++)
     {
-        for (int j = 0; j < i; j++)
-        {
-            cout << ' ';
-        }
-        for (int j = 0; j < (2 * (N - 1)) - (2 * i - 1); j++)
-        {
-            cout << '*';
-        }
-        cout << '\n';
+        cout << string(i,' ')
+        << string(2*(N-1)-(2*i-1),'*')
+        << '\n';
     }
+    
 }
