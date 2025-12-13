c6aa5a658416c449ff69f4d87dcf1b878e8db702
Snow_Rabbit <vega2828@naver.com>
2025-12-13 17:42:15 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string a, b;
+    cin >> a >> b;
+
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+
+    cout << max(stoi(a), stoi(b));
+}
