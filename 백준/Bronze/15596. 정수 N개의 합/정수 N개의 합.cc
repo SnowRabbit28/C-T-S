2a4822f16040f8867af30a800c33c6a1ae029c08
Snow_Rabbit <vega2828@naver.com>
2025-12-23 18:25:34 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 4 ms, Memory: 13740 KB -BaekjoonHub

--- 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.cc
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.cc
@@ -1,11 +1,8 @@
-#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
-long long sum(std::vector<int> &a) {
-	long long ans = 0;
-        for (int i = 0; i < a.size(); i++)
-        {
-            ans += a[i];    
-        }
-	return ans;
+long long sum(vector<int> &a) {
+    long long ans = 0;
+    for (int x : a) ans += x;
+    return ans;
 }
