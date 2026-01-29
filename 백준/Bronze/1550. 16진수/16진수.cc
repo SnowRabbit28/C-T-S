b1894e97edb0662680c5cd913550bd52d2b1494f
Snow_Rabbit <vega2828@naver.com>
2026-01-29 19:34:30 +0900

    [Bronze II] Title: 16진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1550. 16진수/16진수.cc
+++ 백준/Bronze/1550. 16진수/16진수.cc
@@ -1,23 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void)
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    // 16진수는 영어 + 숫자라 string으로 받는다.
-    string s;
-    cin >> s;
-
-    int sum = 0;
-    // 한글자씩 떼서, 기존 값* 16 + c를 해준다.
-    for (char c : s)
-    {
-        if (c - '0' >= 0 && c - '0' <= 9)
-            sum = sum * 16 + (c - '0');
-        else
-            sum = sum * 16 + (c - 'A' + 10);
-    }
-    cout << sum;
+int main(void) {
+    int n;
+    // 입력을 받을 때 "이건 16진수야!"라고 미리 알려주는 방식
+    cin >> hex >> n; 
+    cout << n;
+    return 0;
 }
