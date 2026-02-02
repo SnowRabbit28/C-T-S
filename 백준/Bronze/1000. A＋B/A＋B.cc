4d5bfe3c8f22fe5c65cc158b18c004e801dcccb1
Snow_Rabbit <vega2828@naver.com>
2026-02-02 21:47:22 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int a , b;
-    cin >> a >> b ;
-    cout << a + b;
+    int a,b;
+    cin >> a >> b;
+    cout << a+ b;
 }
