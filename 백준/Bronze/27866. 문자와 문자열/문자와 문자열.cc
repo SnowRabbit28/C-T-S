26909d73d18f5042109d976a1f76d521eae2da34
Snow_Rabbit <vega2828@naver.com>
2026-02-20 20:15:02 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+    int n;
+    cin >> n;
+
+    cout << s[n - 1];
+}
