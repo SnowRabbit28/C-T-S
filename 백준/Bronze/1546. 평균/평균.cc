45d8b2f278494fcee1eaaec095ca4fdcaba5b09a
Snow_Rabbit <vega2828@naver.com>
2026-03-18 20:45:39 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -1,40 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> // max 함수 사용을 위해 필요
-#include <iomanip>   // fixed, setprecision 사용을 위해 필요
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // 1. 입력 속도 향상
+int main(void)
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
+    double sum = 100;
+    double n;
     cin >> n;
 
-    double sum = 0;   // 총합을 저장할 변수 (정밀도를 위해 double)
-    double max_val = 0; // 최댓값을 저장할 변수
-
-    for (int i = 0; i < n; i++) {
-        double score;
-        cin >> score;
-        
-        sum += score; // 일단 모든 점수를 더함
-        if (score > max_val) {
-            max_val = score; // 최댓값 업데이트
-        }
+    vector<double> v;
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        v.push_back(a);
     }
 
-    // 2. 새로운 평균 계산
-    // 공식: (sum / max_val * 100) / n
-    double result = (sum / max_val * 100) / n;
+    sort(v.begin(),v.end());
+    reverse(v.begin(),v.end());
 
-    // 3. 출력 형식 지정 (소수점 고정)
-    cout << fixed;              // 소수점 아래 자릿수를 고정하겠다
-    cout << setprecision(2);    // 소수점 아래 2자리까지 출력하겠다 (반올림 포함)
+    for (int i = 1; i < n; i++)
+    {
+        sum += v[i]/v[0] * 100;
+    }
     
-    cout << result << "\n";
-
-    return 0;
+    cout << sum / n;
+    
 }
+
