4e73bcf9ad00271a8b9a03f0a5996c0fcd8bca04
Snow_Rabbit <vega2828@naver.com>
2026-04-16 13:28:45 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    cout << n + m << "\n";
+    cout << n - m << "\n";
+    cout << n * m << "\n";
+    cout << n / m << "\n";
+    cout << n % m << "\n";
+}
