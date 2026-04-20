353d8f7b4f9d21ecf6208bf60037921fa99b2961
Snow_Rabbit <vega2828@naver.com>
2026-04-20 19:38:29 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.04 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int num1, int num2) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int answer = num1 - num2;
+    return answer;
+}
+
