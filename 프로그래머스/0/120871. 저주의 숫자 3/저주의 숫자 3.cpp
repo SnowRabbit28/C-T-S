6abfef29dec40fff3ad52391fff7d4560de0ac95
Snow_Rabbit <vega2828@naver.com>
2026-06-05 01:30:56 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -5,26 +5,9 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    bool is_three;
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i <= n; i++){
         answer++;
-        while(true){
-            if(answer % 3 == 0){
-                answer++;
-                continue;
-            }
-            is_three = false;
-            int tmp = answer;
-            while(tmp > 0){
-                if(tmp % 10 == 3){
-                    is_three = true;
-                    break;
-                }
-                tmp /= 10;
-            }
-            if(is_three) { answer++; continue; }
-            break;
-        }
+        while (answer % 3 == 0 || to_string(answer).find('3') != string::npos) answer++;
     }
     return answer;
 }
