bb2d5bc3f1178d193a41d391dafe8391cefa2981
Snow_Rabbit <vega2828@naver.com>
2026-08-17 04:08:45 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    int answer = 0;
+    if ( a == b && b == c) {
+        answer = (3 * a) * (3 * (a*a)) * (3 * (a*a*a));
+    }
+    else if(a == b || a == c || b == c){
+        answer = (a + b + c) * ((a*a)+(b*b)+(c*c));
+    }
+    else answer = (a + b + c);
+    return answer;
+}
